4cf4fca853c57f6fc8271cceff7528fb6650ed15
Rob Pupo <pupo.robert@gmail.com>
2021-06-29 18:01:42 -0400

    some tweaks to clean routes syntax errors

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -4,13 +4,12 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 
-
 router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
-    const tag = await Tag.findall({
-      include: [{model: Product}]
+    const tag = await Tag.findAll({
+      include: [{model: Product, through: ProductTag}]
     });
     res.status(200).json(tag);
   } catch (err) {
@@ -22,10 +21,10 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-    const tag = await Tag.findall({
-      include: [{model: Product}]
+    const tag = await Tag.findByPk(req.params.id, {
+      include: [{model: Product, through: ProductTag}]
     });
-    res.status(200).json(tag);
+    res.status(200).json(tag)
   } catch (err) {
     res.status(500).json(err);
   }
